fff636be78af66fa6919dda0e7a092e7e88afd3c
MauricioGonzalezG <mauricio.gonzalez.kivio@gmail.com>
2024-12-16 09:03:50 -0500

    Added plugins custom, replace logic on NopCommerce project and added custom plugin example

---
+++ CustomEcommerce/NopCommerce/Plugins/Nop.Plugin.Payments.ExamplePlugin/Models/ConfigurationModel.cs
@@ -0,0 +1,45 @@
+﻿using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
+
+namespace Nop.Plugin.Payments.ExamplePlugin.Models;
+
+public record ConfigurationModel : BaseNopModel, ILocalizedModel<ConfigurationModel.ConfigurationLocalizedModel>
+{
+    public ConfigurationModel()
+    {
+        Locales = new List<ConfigurationLocalizedModel>();
+    }
+
+    public int ActiveStoreScopeConfiguration { get; set; }
+
+    [NopResourceDisplayName("Plugins.Payment.ExamplePlugin.DescriptionText")]
+    public string DescriptionText { get; set; }
+    public bool DescriptionText_OverrideForStore { get; set; }
+
+    [NopResourceDisplayName("Plugins.Payment.ExamplePlugin.AdditionalFee")]
+    public decimal AdditionalFee { get; set; }
+    public bool AdditionalFee_OverrideForStore { get; set; }
+
+    [NopResourceDisplayName("Plugins.Payment.ExamplePlugin.AdditionalFeePercentage")]
+    public bool AdditionalFeePercentage { get; set; }
+    public bool AdditionalFeePercentage_OverrideForStore { get; set; }
+
+    [NopResourceDisplayName("Plugins.Payment.ExamplePlugin.ShippableProductRequired")]
+    public bool ShippableProductRequired { get; set; }
+    public bool ShippableProductRequired_OverrideForStore { get; set; }
+
+    public IList<ConfigurationLocalizedModel> Locales { get; set; }
+
+    #region Nested class
+
+    public class ConfigurationLocalizedModel : ILocalizedLocaleModel
+    {
+        public int LanguageId { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payment.ExamplePlugin.DescriptionText")]
+        public string DescriptionText { get; set; }
+    }
+
+    #endregion
+
+}
---
+++ CustomEcommerce/NopCommerce/Plugins/Nop.Plugin.Payments.ExamplePlugin/Models/PaymentInfoModel.cs
@@ -0,0 +1,8 @@
+﻿using Nop.Web.Framework.Models;
+
+namespace Nop.Plugin.Payments.ExamplePlugin.Models;
+
+public record PaymentInfoModel : BaseNopModel
+{
+    public string DescriptionText { get; set; }
+}
