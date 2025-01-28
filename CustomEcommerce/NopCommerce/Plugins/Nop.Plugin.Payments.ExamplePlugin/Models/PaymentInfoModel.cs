ed51e023ae195632385911954551a35a498a3ffd
Andres Arnedo <andresarnedo2002@gmail.com>
2025-01-28 12:20:50 -0500

    feat(Shipping.FixedByWeightByTotal): add new shipping plugin and remove example payment plugin

--- CustomEcommerce/NopCommerce/Plugins/Nop.Plugin.Payments.ExamplePlugin/Models/PaymentInfoModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using Nop.Web.Framework.Models;
-
-namespace Nop.Plugin.Payments.ExamplePlugin.Models;
-
-public record PaymentInfoModel : BaseNopModel
-{
-    public string DescriptionText { get; set; }
-}
