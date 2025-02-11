dee6bae15698d0ed8a81e4c9c8827eaa8481742e
MauricioGonzalezG <mauricio.gonzalez.kivio@gmail.com>
2025-02-11 12:06:56 -0500

    Added Plugin.Tests project

---
+++ CustomEcommerce/NopCommerce/Plugins/Plugins.Tests/Plugin.Tests/CashOnDeliveryPaymentProcessorTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Plugins.Tests.Plugin.CashOnDelivery
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CashOnDeliveryPaymentProcessorTests
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.DoesNotThrow(() => { });
+        }
+    }
+}
