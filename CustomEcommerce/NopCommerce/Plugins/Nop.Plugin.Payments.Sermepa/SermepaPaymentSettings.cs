2f9512edd2bb8299e10a3ce502b75ae299589610
sebasKivio <sebastian@kivio.com.co>
2025-02-18 18:41:40 -0500

    Sermepa-UnitTests
    Unit test for methods in the Processor class of Sermepa.

--- CustomEcommerce/NopCommerce/Plugins/Nop.Plugin.Payments.Sermepa/SermepaPaymentSettings.cs
+++ CustomEcommerce/NopCommerce/Plugins/Nop.Plugin.Payments.Sermepa/SermepaPaymentSettings.cs
@@ -7,13 +7,13 @@ namespace Nop.Plugin.Payments.Sermepa
         public string NombreComercio { get; set; }
         public string Titular { get; set; }
         public string Producto { get; set; }
-        public string FUC { get; set; }
-        public string Terminal { get; set; }
-        public string Moneda { get; set; }
+        public virtual string FUC { get; set; }
+        public virtual string Terminal { get; set; }
+        public virtual string Moneda { get; set; }
         public string ClaveReal { get; set; }
-        public string ClavePruebas { get; set; }
-        public bool Pruebas { get; set; }
-        public decimal AdditionalFee { get; set; }
+        public virtual string ClavePruebas { get; set; }
+        public virtual bool Pruebas { get; set; }
+        public virtual decimal AdditionalFee { get; set; }
         public bool AdditionalFeePercentage { get; set; }
     }
 }
