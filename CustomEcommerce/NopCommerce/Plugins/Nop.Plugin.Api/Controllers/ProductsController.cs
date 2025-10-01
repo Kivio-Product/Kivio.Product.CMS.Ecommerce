7b27ee13a5921487a46d9d25cbebf9c1538a0d52
Mauricio <176711228+MauricioGonzalezG@users.noreply.github.com>
2025-10-01 17:21:08 -0500

    Add omitValidationPrice to bypass price update check

--- CustomEcommerce/NopCommerce/Plugins/Nop.Plugin.Api/Controllers/ProductsController.cs
+++ CustomEcommerce/NopCommerce/Plugins/Nop.Plugin.Api/Controllers/ProductsController.cs
@@ -391,8 +391,13 @@ namespace Nop.Plugin.Api.Controllers
             }
 
             var priceUpdateConditionEnabled = await _settingService.GetSettingByKeyAsync<bool>("ApiSettings.PriceUpdateConditionEnabled", false);
+            var omitValidationPrice = productDelta.Dto.OmitValidationPrice ?? false;
 
-            if (priceUpdateConditionEnabled && productDelta.Dto.Price.HasValue && productDelta.Dto.Price > 0 && productDelta.Dto.Price < product.Price)
+            if (priceUpdateConditionEnabled &&
+            productDelta.Dto.Price.HasValue &&
+            productDelta.Dto.Price > 0 &&
+            productDelta.Dto.Price < product.Price &&
+            !omitValidationPrice)
             {
                 return Ok(new { message = "Price update condition not met" });
             }
