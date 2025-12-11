cc1213b205a063433a9ea6cfeff06a25f20d0e2f
Mauricio <176711228+MauricioGonzalezG@users.noreply.github.com>
2025-12-11 15:54:56 -0500

    Fix product ID reference in shipping check

--- CustomEcommerce/NopCommerce/Plugins/Nop.Plugin.Api/Controllers/OrdersController.cs
+++ CustomEcommerce/NopCommerce/Plugins/Nop.Plugin.Api/Controllers/OrdersController.cs
@@ -411,7 +411,7 @@ namespace Nop.Plugin.Api.Controllers
 
             var customer = await CustomerService.GetCustomerByIdAsync(currentOrder.CustomerId);
 
-            var shippingRequired = await (await _orderService.GetOrderItemsAsync(currentOrder.Id)).AnyAwaitAsync(async item => !(await _productService.GetProductByIdAsync(item.Id)).IsFreeShipping);
+            var shippingRequired = await (await _orderService.GetOrderItemsAsync(currentOrder.Id)).AnyAwaitAsync(async item => !(await _productService.GetProductByIdAsync(item.ProductId)).IsFreeShipping);
 
             if (shippingRequired)
             {
